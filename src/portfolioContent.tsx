fed3f476223cdae4cc246f58d56ee2ff3f613150
TimeSentinel <lance.stubblefield@gmail.com>
2024-10-31 18:47:14 -0700

    worked on filmstrip reformatted images

--- src/portfolioContent.tsx
+++ src/portfolioContent.tsx
@@ -1,18 +1,23 @@
-
+import mainBG from './images/079.webp'
 function MainContent() {
 
 
     return (
         <>
             <div className="mainHead">
-                <h1>Main Page</h1>
+                <h1>Specialized Web Design</h1>
+                <p>
+
+                </p>
             </div>
             <div className="mainBody">
                 Main Body
             </div>
             <div className="mainFoot">
-
-
+                <h2>Thanks for checking out my site!</h2>
+                <div className="imageFrame">
+                    <img src={mainBG} alt="background image" />
+                </div>
             </div>
 
         </>

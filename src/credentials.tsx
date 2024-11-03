919a8bde8ed266ec6d96ee52e873b0769af8d1ee
TimeSentinel <lance.stubblefield@gmail.com>
2024-11-03 15:29:31 -0800

    Added resume page framework

--- src/credentials.tsx
+++ src/credentials.tsx
@@ -3,9 +3,12 @@ function Credentials() {
         <>
             <div className="mainHead">
                 <h1>Demonstrations of My Skills</h1>
-                <p>
-                   This is like my online resume. Here you'll find my credentials, references, and skills.
-                </p>
+                <h2>Profiles</h2>
+                View my code on <a href="https://github.com/TimeSentinel">GitHub</a><br/>
+                Connect with me on <a href="https://www.linkedin.com/in/lance-stubblefield-designer/">LinkedIn</a><br/>
+                Hire me on <a href="https://www.upwork.com/freelancers/timesentinel">Upwork</a><br/>
+                Watch my videos on <a href="https://www.youtube.com/@Sol3-l5j">youTube</a><br/>
+
             </div>
             <div className="mainBody">
 
